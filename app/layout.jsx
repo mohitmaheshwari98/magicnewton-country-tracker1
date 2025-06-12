460c7decb660f70028e72f8340c8d9d7f4bef620
Mohit Maheshwari <mohitmaheshwari148@gmail.com>
2025-06-12 15:48:13 +0200

    Fix filename: layout.jsx instead of layout.jsx.txt

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'MagicNewton Country Tracker',
+  description: 'See where the MagicNewton army is from 🌍',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
